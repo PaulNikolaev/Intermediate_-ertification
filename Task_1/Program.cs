2523d4e51fb5c30f5677ce43d38c743ec385c9db
Poul Nikolaev <panikolaev7@gmail.com>
2024-03-07 13:57:02 +0300

    add Task_1

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. 
+// Использовать рекурсию, не использовать циклы.
+
+
+Console.Clear();
+
+void OutputNaturalNumbers(int m, int n)
+{
+    if (n == m - 1)
+    {
+        return;
+    }
+    OutputNaturalNumbers(m, n - 1);
+    Console.Write(n+" ");
+}
+
+Console.Write("Введите минимальное число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите максимальное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+OutputNaturalNumbers(m, n);
+
+
